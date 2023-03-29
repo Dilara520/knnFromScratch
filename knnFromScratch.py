cebadf5331d1edf2ff195bc09f35c85a42fa3d43
Dilara Cakmak <81981117+Dilara520@users.noreply.github.com>
2023-03-29 17:07:37 +0300

    KNN from scratch w/ numpy and pandas

---
+++ knnFromScratch.py
@@ -0,0 +1,127 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing
+import matplotlib.pyplot as plt #plotting
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+# calculate the Euclidean distance between two vectors
+from math import sqrt
+def euclidean_distance(row1, row2):
+	distance = 0.0
+	for i in range(len(row1)-1):
+		distance += (row1[i] - row2[i])**2
+	return sqrt(distance)
+
+# Example of calculating Euclidean distance
+# Test distance function
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+row0 = dataset[0]
+for row in dataset:
+	distance = euclidean_distance(row0, row)
+	print(distance)
+
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = euclidean_distance(test_row, train_row)
+		distances.append((train_row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+# Test distance function
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+neighbors = get_neighbors(dataset, dataset[0], 3)
+for neighbor in neighbors:
+	print(neighbor)
+
+# Make a classification prediction with neighbors
+def predict_classification(train, test_row, num_neighbors):
+	neighbors = get_neighbors(train, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+prediction = predict_classification(dataset, dataset[0], 3)
+print('Expected %d, Got %d.' % (dataset[0][-1], prediction))
+
+# Load iris data and store in dataframe
+iris = datasets.load_iris()
+
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+df.head()
+df_list = df.values.tolist()
+
+test_pt = [5.1, 3.5, 1.4, 0.2, 0.0]
+
+for row in df_list:
+	distance = euclidean_distance(test_pt, row)
+	print(distance)
+
+neighbors = get_neighbors(df_list, test_pt, 3)
+for neighbor in neighbors:
+	print(neighbor)
+
+prediction = predict_classification(df_list, test_pt, 3)
+print('Expected %d, Got %d.' % (test_pt[-1], prediction))
+
+# split into train test sets
+train, test = train_test_split(df, test_size=0.25, random_state=0)
+y_test = test.target
+train_list = train.values.tolist()
+test_list = test.values.tolist()
+y_test_list = y_test.values.tolist()
+
+# kNN Algorithm
+def k_nearest_neighbors(train, test, num_neighbors):
+	predictions = list()
+	for row in test:
+		output = predict_classification(train, row, num_neighbors)
+		predictions.append(output)
+	return(predictions)
+
+num_neighbors = 3
+predictions = k_nearest_neighbors(train_list, test_list, num_neighbors)
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+acc = accuracy_metric(y_test_list, predictions)
+acc
